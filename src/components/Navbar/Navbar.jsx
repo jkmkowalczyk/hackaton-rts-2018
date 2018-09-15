14e0028d8a1959f06d645be68d5c3d1854cd8c34
Jakub Kowalczyk <jkm.kowalczyk@gmail.com>
2018-09-15 05:24:17 +0200

    some navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import avatar from "./default-avatar.png";
 import fire from "../../fire";
 import Sticky from "react-sticky-el";
 
+
 class Navbar extends Component {
 
     state = {
@@ -28,33 +29,32 @@ class Navbar extends Component {
         this.props.history.push("/dashboard");
     };
 
+    rotate = () => {
+        let img = document.getElementById('image');
+        img.setAttribute('style', 'transform:rotate(180deg)');
+    };
+
     render() {
         return (
             <Sticky>
-                <div className="navbar">
-                    <Menu>
-                        <Menu.Item onClick={this.handleItemClick}>
-                            <div className="navbar-logo"/>
+                <Menu>
+                    <Menu.Item onClick={this.handleItemClick}>
+                        <div className="navbar-logo"/>
+                        <p className="navbar-title">REACT TESTING SERVICE</p>
+                    </Menu.Item>
+                    <Menu.Menu position="right">
+                        <Menu.Item>
+                            <Searchbar/>
                         </Menu.Item>
                         <Menu.Item>
-                            <p className="navbar-title">REACT TESTING SERVICE</p>
+                            <img className="navbar-user-container-avatar" src={avatar}/>
+                            <div className="navbar-user-container-name">
+                                Cześć {this.state.userName}
+                            </div>
                         </Menu.Item>
-
-                        <Menu.Menu position="right">
-                            <Menu.Item>
-                                <Searchbar/>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <img className="navbar-user-container-avatar" src={avatar}/>
-                                <div className="navbar-user-container-name">
-                                    {" "}
-                                    Cześć {this.state.userName}
-                                </div>
-                            </Menu.Item>
-                            <Menu.Item onClick={this.handleLogoutClick}>Wyloguj</Menu.Item>
-                        </Menu.Menu>
-                    </Menu>
-                </div>
+                        <Menu.Item onClick={this.handleLogoutClick}>Wyloguj</Menu.Item>
+                    </Menu.Menu>
+                </Menu>
             </Sticky>
         );
     }
