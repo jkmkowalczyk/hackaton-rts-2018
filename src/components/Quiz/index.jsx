6d5bb123b54ef76eb0b465bf28709bcc8162bd8e
jrozbicki <j.rozbicki9@gmail.com>
2018-09-15 05:44:47 +0200

    push

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,32 +1,10 @@
-import React, {Component} from 'react';
-import fire from '../../fire';
-
+import React, { Component } from "react";
+import fire from "../../fire";
 
 class Quiz extends Component {
-
-
-    q;
-    c;
-
-    get = () => {
-        fire.database().ref("tests/" + "test1").on("value", snapshot => {
-            this.q = snapshot.val().pytania;
-            this.c = snapshot.val().category;
-        })
-    };
-
-    componentWillMount() {
-        this.get()
-    };
-
-
-    render() {
-        return (
-            <div>
-                {console.log(this.q)}
-            </div>
-        )
-    }
+  render() {
+    return <div>{this.props.test.name}</div>;
+  }
 }
 
-export default Quiz
+export default Quiz;
