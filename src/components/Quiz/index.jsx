f2868805877c49fc0338a0590a8dc51d6e52bb71
jrozbicki <j.rozbicki9@gmail.com>
2018-09-15 08:23:37 +0200

    added questions

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import fire from "../../fire";
+import _ from "lodash";
 
 class Quiz extends Component {
   constructor(props) {
@@ -17,16 +18,36 @@ class Quiz extends Component {
       .equalTo(name)
       .once("value")
       .then(snapshot => {
-        console.log(" quiz ", snapshot.val());
+        console.log("quiz did mount", snapshot.val());
         this.setState({
           quiz: snapshot.val()
         });
       });
   }
+  renderQuestion(question) {
+    console.log("question", question);
+    return (
+      <div>
+        <div>Pytanie: {question.pyt}</div>
+        <div>Odpowiedz A: {question.odpa}</div>
+        <div>Odpowiedz B: {question.odpb}</div>
+        <div>Odpowiedz C: {question.odpc}</div>
+        <div>Odpowiedz D: {question.odpd}</div>
+      </div>
+    );
+  }
+
+  renderTest() {
+    if (!Array.isArray(this.state.quiz)) {
+      const test = this.state.quiz[Object.keys(this.state.quiz)[0]];
+      return _.map(test.pytania, obj => {
+        return this.renderQuestion(obj);
+      });
+    }
+  }
 
   render() {
-    console.log("quiz", this.state.quiz);
-    return <div>{this.state.quiz}</div>;
+    return <div>{this.renderTest()}</div>;
   }
 }
 
