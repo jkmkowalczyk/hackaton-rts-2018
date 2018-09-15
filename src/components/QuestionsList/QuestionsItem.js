9a3882254a64f47fda3db33d440d1e86932b0084
micavanco <micavanco@gmail.com>
2018-09-15 02:30:38 +0200

    Fixed radio button handler and answer text

--- src/components/QuestionsList/QuestionsItem.js
+++ src/components/QuestionsList/QuestionsItem.js
@@ -10,25 +10,25 @@ export default class QuestionsItem extends Component
             clicked: '',
             array: [
                 <input id={"pyt"+this.props.Key} key={"pyt"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz pytanie...")} placeholder="Wpisz pytanie..."></input>,
-                <form>
-                    <div className="input-box">
+                <form  key={"f"+this.props.Key}>
+                    <div className="input-box" key={"a"+this.props.Key}>
                         <input type="radio" onClick={this.OnRadioClick.bind(this)} key={"ra"+this.props.Key} value="A" name="answer"></input>
                         <input id={"odpa"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź a...")} key={"odpa"+this.props.Key} placeholder="Wpisz odpowiedź a..."></input>
                     </div>
-                    <div className="input-box">
+                    <div className="input-box" key={"b"+this.props.Key}>
                         <input type="radio" onClick={this.OnRadioClick.bind(this)} value="B" name="answer" key={"rb"+this.props.Key}></input>
                         <input id={"odpb"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź b...")} key={"odpb"+this.props.Key} placeholder="Wpisz odpowiedź b..."></input>
                     </div>
-                    <div className="input-box">
+                    <div className="input-box" key={"c"+this.props.Key}>
                         <input type="radio" onClick={this.OnRadioClick.bind(this)} value="C" name="answer" key={"rc"+this.props.Key}></input>
                         <input id={"odpc"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź c...")} key={"odpc"+this.props.Key} placeholder="Wpisz odpowiedź c..."></input>
                     </div>
-                    <div className="input-box">
+                    <div className="input-box" key={"d"+this.props.Key}>
                         <input type="radio" onClick={this.OnRadioClick.bind(this)} value="D" name="answer" key={"rd"+this.props.Key}></input>
                         <input id={"odpd"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź d...")} key={"odpd"+this.props.Key} placeholder="Wpisz odpowiedź d..."></input>
                     </div>
                 </form>,
-                <div>Prawidłowa odpowiedź: </div>],
+                <div  key={"p"+this.props.Key}>Prawidłowa odpowiedź: <p id={"odp"+this.props.Key} key={"ans"+this.props.Key}></p></div>],
             lastClicked: null
         };
     }
@@ -47,26 +47,26 @@ export default class QuestionsItem extends Component
     {
         if(this.state.lastClicked) this.state.lastClicked.style.borderColor = "#dddddd";
         this.setState({array: [
-        <input id={"pyt"+this.props.Key} key={"pyt"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz pytanie...")} placeholder="Wpisz pytanie..."></input>,
-        <form>
-            <div className="input-box">
-                <input type="radio" onClick={this.OnRadioClick.bind(this)} key={"ra"+this.props.Key} value="A" name="answer"></input>
-                <input id={"odpa"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź a...")} key={"odpa"+this.props.Key} placeholder="Wpisz odpowiedź a..."></input>
-            </div>
-            <div className="input-box">
-                <input type="radio" onClick={this.OnRadioClick.bind(this)} value="B" name="answer" key={"rb"+this.props.Key}></input>
-                <input id={"odpb"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź b...")} key={"odpb"+this.props.Key} placeholder="Wpisz odpowiedź b..."></input>
-            </div>
-            <div className="input-box">
-                <input type="radio" onClick={this.OnRadioClick.bind(this)} value="C" name="answer" key={"rc"+this.props.Key}></input>
-                <input id={"odpc"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź c...")} key={"odpc"+this.props.Key} placeholder="Wpisz odpowiedź c..."></input>
-            </div>
-            <div className="input-box">
-                <input type="radio" onClick={this.OnRadioClick.bind(this)} value="D" name="answer" key={"rd"+this.props.Key}></input>
-                <input id={"odpd"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź d...")} key={"odpd"+this.props.Key} placeholder="Wpisz odpowiedź d..."></input>
-            </div>
-        </form>,
-        <div>Prawidłowa odpowiedź: {e.target.value}</div>]});
+                <input id={"pyt"+this.props.Key} key={"pyt"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz pytanie...")} placeholder="Wpisz pytanie..."></input>,
+                <form  key={"f"+this.props.Key}>
+                    <div className="input-box" key={"a"+this.props.Key}>
+                        <input type="radio" onClick={this.OnRadioClick.bind(this)} key={"ra"+this.props.Key} value="A" name="answer"></input>
+                        <input id={"odpa"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź a...")} key={"odpa"+this.props.Key} placeholder="Wpisz odpowiedź a..."></input>
+                    </div>
+                    <div className="input-box" key={"b"+this.props.Key}>
+                        <input type="radio" onClick={this.OnRadioClick.bind(this)} value="B" name="answer" key={"rb"+this.props.Key}></input>
+                        <input id={"odpb"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź b...")} key={"odpb"+this.props.Key} placeholder="Wpisz odpowiedź b..."></input>
+                    </div>
+                    <div className="input-box" key={"c"+this.props.Key}>
+                        <input type="radio" onClick={this.OnRadioClick.bind(this)} value="C" name="answer" key={"rc"+this.props.Key}></input>
+                        <input id={"odpc"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź c...")} key={"odpc"+this.props.Key} placeholder="Wpisz odpowiedź c..."></input>
+                    </div>
+                    <div className="input-box" key={"d"+this.props.Key}>
+                        <input type="radio" onClick={this.OnRadioClick.bind(this)} value="D" name="answer" key={"rd"+this.props.Key}></input>
+                        <input id={"odpd"+this.props.Key} onFocus={this.OnInput} onBlur={(e) => this.OnInputBlur(e, "Wpisz odpowiedź d...")} key={"odpd"+this.props.Key} placeholder="Wpisz odpowiedź d..."></input>
+                    </div>
+                </form>,
+                <div  key={"p"+this.props.Key}>Prawidłowa odpowiedź: <p id={"odp"+this.props.Key} key={"ans"+this.props.Key}>{e.target.value}</p></div>]});
         e.target.nextElementSibling.style.borderColor = "green";
         this.state.lastClicked=e.target.nextElementSibling;
     }
--- src/components/QuestionsList/QuestionsList.js
+++ src/components/QuestionsList/QuestionsList.js
@@ -19,20 +19,26 @@ export default class QuestionsList extends Component
 
     BtnClick(e)
     {
-        if(document.getElementById("pyt"+(this.state.i-1).toString()).value !== '' &&
-            document.getElementById("odpa"+(this.state.i-1).toString()).value !== '' &&
-            document.getElementById("odpb"+(this.state.i-1).toString()).value !== '' &&
-            document.getElementById("odpc"+(this.state.i-1).toString()).value !== '' &&
-            document.getElementById("odpd"+(this.state.i-1).toString()).value !== '' &&
-            document.getElementById("odp"+(this.state.i-1).toString()).value !== ''
+        let temp = document.getElementById("pyt"+(this.state.i-1).toString()).value;
+        let temp2 = document.getElementById("odpa"+(this.state.i-1).toString()).value;
+        let temp3 = document.getElementById("odpb"+(this.state.i-1).toString()).value;
+        let temp4 = document.getElementById("odpc"+(this.state.i-1).toString()).value;
+        let temp5 = document.getElementById("odpd"+(this.state.i-1).toString()).value;
+        let temp6 = document.getElementById("odp"+(this.state.i-1).toString()).innerText;
+        if(temp !== '' &&
+            temp2 !== '' &&
+            temp3 !== '' &&
+            temp4 !== '' &&
+            temp5 !== '' &&
+            temp6 !== ''
         )
         {
-            this.state.array.push({pyt: document.getElementById("pyt"+(this.state.i-1).toString()).value,
-                odpa: document.getElementById("odpa"+(this.state.i-1).toString()).value,
-                odpb: document.getElementById("odpa"+(this.state.i-1).toString()).value,
-                odpc: document.getElementById("odpa"+(this.state.i-1).toString()).value,
-                odpd: document.getElementById("odpa"+(this.state.i-1).toString()).value,
-                odp: document.getElementById("odpa"+(this.state.i-1).toString()).value});
+            this.state.array.push({pyt: temp,
+                odpa: temp2,
+                odpb: temp3,
+                odpc: temp4,
+                odpd: temp5,
+                odp: temp6});
             this.setState({questions: <QuestionsItem key={"question"+this.state.i} Key={this.state.i}/>});
             this.state.i=this.state.i+1;
             document.querySelector(".category-select").disabled = true;
@@ -44,13 +50,13 @@ export default class QuestionsList extends Component
         return(
             <div className="wrapper">
                 <CategoryList/>
-                {this.state.array.map(i => {if(i)return (<div className="questions-submited">
-                    <div className="q-submited">Pytanie: {i.pyt}</div>
-                    <div className="q-submited">Odpowiedź A: {i.odpa}</div>
-                    <div className="q-submited">Odpowiedź B: {i.odpb}</div>
-                    <div className="q-submited">Odpowiedź C: {i.odpc}</div>
-                    <div className="q-submited">Odpowiedź D: {i.odpd}</div>
-                    <div className="q-submited">Odpowiedź Prawidłowa: {i.odp}</div>
+                {this.state.array.map(i => {if(i)return (<div key={"qs"+this.state.i} className="questions-submited">
+                    <div key={"op"+this.state.i} className="q-submited">Pytanie: {i.pyt}</div>
+                    <div key={"oa"+this.state.i} className="q-submited">Odpowiedź A: {i.odpa}</div>
+                    <div key={"ob"+this.state.i} className="q-submited">Odpowiedź B: {i.odpb}</div>
+                    <div key={"oc"+this.state.i} className="q-submited">Odpowiedź C: {i.odpc}</div>
+                    <div key={"od"+this.state.i} className="q-submited">Odpowiedź D: {i.odpd}</div>
+                    <div key={"a"+this.state.i} className="q-submited">Odpowiedź Prawidłowa: {i.odp}</div>
                    </div>)})}
                 <div className="questions-container">{this.state.questions}</div>
                 <button id="next-btn" onClick={this.BtnClick.bind(this)}>Następny</button>
